7302193a7ddba5a593cf4eb0802e47c1ad2f0333
jeasong <cjs5241@gmail.com>
2021-05-17 15:33:17 +0900

    type:서버셋팅 hello world #004

---
+++ src/app.ts
@@ -0,0 +1,47 @@
+import express, { Application } from 'express';
+import https from 'https';
+import fs from 'fs';
+import morgan from 'morgan';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const port: number = 4000;
+const app: Application = express();
+const clientAddr = process.env.CLIENT_ADDR || 'https://localhost:3000'
+
+app.use(morgan('dev'));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cors(
+    {
+        origin: [clientAddr],
+        credentials: true,
+        methods: ["GET", "POST", "OPTIONS", "DELETE", "PUT"]
+    }
+    ));
+app.use(cookieParser());
+
+app.get(
+    "/",
+    (req: express.Request, res: express.Response, next: express.NextFunction) => {
+        res.send("hello typescript express!");
+    }
+);
+
+let server;
+
+if (fs.existsSync('./key.pem') && fs.existsSync('./cert.pem')) {
+    server = https.createServer(
+        {
+            key: fs.readFileSync('./key.pem', 'utf-8'),
+            cert: fs.readFileSync ('./cert.pem', 'utf-8')
+        },
+        app
+    ).listen(port, () => {
+        console.log('https server on ' + port);
+        })
+};
