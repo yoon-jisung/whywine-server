d91b063d60c4456e39046d592505c9e748ec4ab0
Youjin Lee <youjinlee1997@gmail.com>
2021-05-19 22:11:56 +0900

    기능: 설문조사 완료 후 태그 업데이트 요청 / #10

--- src/app.ts
+++ src/app.ts
@@ -6,16 +6,11 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 import "reflect-metadata";
-import { createConnection } from "typeorm";
-import ormconfig from "../ormconfig";
-const connection = createConnection(ormconfig);
+import indexRouter from "./routers/index";
+import authRouter from "./routers/auth";
+import userRouter from "./routes/user";
 
 dotenv.config();
-
-import index from './routers/index';
-import auth from './routers/auth';
-
-
 const port: number = 4000;
 const app: Application = express();
 const clientAddr = process.env.CLIENT_ADDR || "https://localhost:3000";
@@ -34,8 +29,9 @@ app.use(
 );
 app.use(cookieParser());
 
-app.use('/', index);
-app.use('/auth', auth);
+app.use("/", indexRouter);
+app.use("/auth", authRouter);
+app.use("/user", userRouter);
 
 let server;
 
