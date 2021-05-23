6d736456fc8234ae6f65b42e805936e2330cfeaa
jeasong <cjs5241@gmail.com>
2021-05-24 02:26:58 +0900

    디버깅:auth,userinfo모든 버그 수정

--- src/controllers/auth/signup.ts
+++ src/controllers/auth/signup.ts
@@ -10,6 +10,7 @@ dotenv.config();
 
 const signup = async (req: Request, res: Response, next: NextFunction) => {
     const userRepository = await getRepository(User);
+    
     if (req.body.nickname === '' || req.body.email === '' || req.body.password === ''){
         return res.status(400).json({ data: null,  message: "fill email, password and nickname" })
     }
@@ -17,7 +18,7 @@ const signup = async (req: Request, res: Response, next: NextFunction) => {
     const email:string = req.body.email;
     const nickname:string = req.body.email;
     const password:string = req.body.password;
-    const tags:number[] = req.body.tags
+    const tags:string[] = req.body.tags
     const saltedPassword = password + process.env.SALT
     const hashPassword = crypto
     .createHmac('sha512', process.env.CRYPTOKEY!)
@@ -29,23 +30,28 @@ const signup = async (req: Request, res: Response, next: NextFunction) => {
     if(findEmail){
         return res.status(401).json({ data: null, message: "email already existed"});
     }
-    const userInfo:object = {
-        email,nickname,hashPassword,tags
-    }
-    await userRepository.save(userInfo)
-
-    const accessToken = jwt.sign(
-        userInfo,
-        process.env.ACCTOKEN_SECRET!,
-        { expiresIn: '30m' }
-    );
-    const refreshToken = jwt.sign(
-        userInfo,
-        process.env.REFTOKEN_SECRET!,
-        { expiresIn: '1h' }
-    );
-
-    return res
+    const userInfo = new User
+    userInfo.email = email
+    userInfo.nickname = nickname
+    userInfo.password = hashPassword
+    userInfo.likes = 0
+    userInfo.image = ''
+    //userInfo.tags = tags
+    const saveData = await userRepository.save(userInfo)
+    console.log('저장됨',saveData)
+    console.log('userInfo-------',userInfo)
+    try {
+        const accessToken:string = jwt.sign(
+            {userInfo},
+            process.env.ACCTOKEN_SECRET!,
+            { expiresIn: '30m' }
+        );
+        const refreshToken:string = jwt.sign(
+            {userInfo},
+            process.env.REFTOKEN_SECRET!,
+            { expiresIn: '1h' }
+        );
+        return res
         .status(201)
         .cookie('refreshToken', refreshToken, {
             path: '/',
@@ -53,5 +59,8 @@ const signup = async (req: Request, res: Response, next: NextFunction) => {
             secure: true,
             sameSite: 'none'
         }).json({ data: accessToken,  message: 'ok' })
+    } catch (error) {
+        console.log(error)
+    }
 }
 export default signup;
