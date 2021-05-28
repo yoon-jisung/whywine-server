e98bc493c4bfa3491172e2651b8e64c790b4be84
Youjin Lee <youjinlee1997@gmail.com>
2021-05-28 21:36:17 +0900

    중간 커밋

--- src/controllers/comment.ts
+++ src/controllers/comment.ts
@@ -6,37 +6,191 @@ import { Wine } from "../entity/wine";
 import jwt from "jsonwebtoken";
 import { User } from "../entity/user";
 import { Recomment } from "../entity/recomment";
+import axios from "axios";
 require("dotenv").config();
+interface TokenInterface {
+  // verified accessToken의 인터페이스
+  id: number;
+  email: string;
+  nickname: string;
+  likes?: number;
+  image?: Buffer;
+  tags: Tag[];
+  good?: Comment[];
+  bad?: Comment[];
+  wines?: Wine[];
+}
 
+interface CommentInterface {
+  id: number;
+  text: string;
+  userId?: number;
+  wineId?: number;
+  good_count: number;
+  bad_count: number;
+  recomments?: Recomment[];
+}
 export = {
-  good: async (req: Request, res: Response) => {
+  post: async (req: Request, res: Response) => {
     const connection = getConnection();
     const userRepo = await connection.getRepository(User);
+    const wineRepo = await connection.getRepository(Wine);
     const commentRepo = await connection.getRepository(Comment);
-    const { commentId } = req.body;
+    const { wineId, text } = req.body;
 
-    // const userId: number = req.session!.passport!.user;
-    const userId = 2;
-    const user = await userRepo.findOne({
-      where: { id: userId },
-      relations: ["good"],
-    });
-    const comment = await commentRepo.findOne({ id: commentId });
+    const userId: number = req.session!.passport!.user;
 
-    if (user && comment) {
-      // user.good = [...user.good, comment];
-      // comment.good_count++;
+    const user = await userRepo.findOne({ id: userId });
+    const wine = await wineRepo.findOne({ id: wineId });
+    if (user === undefined || wine === undefined || text === "") {
+      res
+        .status(404)
+        .send({ message: "text, wineId or accessToken not existed" });
+      return;
+    } else {
+      let comment = new Comment();
+      comment.text = text;
+      comment.user = user;
+      comment.wine = wine;
+      comment.good_count = 0;
+      comment.bad_count = 0;
+      const newComment = await connection.manager.save(comment);
 
-      // await connection.manager.save(user);
-      // await connection.manager.save(comment);
-
-      console.log(user);
       res.status(200).send({
         data: {
-          goodCount: comment.good_count,
+          newComment,
         },
+        message: "ok.",
       });
+      return;
+    }
+  },
+  get: async (req: Request, res: Response) => {
+    try {
+      const connection = getConnection();
+      const userRepo = await connection.getRepository(User);
+      const wineRepo = await connection.getRepository(Wine);
+      const commentRepo = await connection.getRepository(Comment);
+      const recommentRepo = await connection.getRepository(Recomment);
+
+      const wineId: number = Number(req.query.wineid);
+      const userId: number = req.session!.passport!.user;
+
+      const user = await userRepo.findOne({
+        where: { id: userId },
+        relations: ["good", "bad"],
+      });
+      const wine = await wineRepo.findOne({ id: wineId });
+      if (user && wine) {
+        let comments: Comment[] = await commentRepo.find({
+          where: { wine: wineId },
+          relations: ["user", "wine"],
+        });
+        let results: CommentInterface[] = [];
+        for (let c of comments) {
+          let res: CommentInterface = {
+            id: c.id,
+            text: c.text,
+            userId: c.user.id,
+            wineId: c.wine.id,
+            good_count: c.good_count,
+            bad_count: c.bad_count,
+            recomments: await recommentRepo.find({ where: { comment: c.id } }),
+          };
+          results.push(res);
+        }
+        const usersgood: number[] = user.good.map((comment) => comment.id);
+        const usersbad: number[] = user.bad.map((comment) => comment.id);
+        res.status(200).send({
+          data: {
+            comments: results,
+            usersgood,
+            usersbad,
+          },
+          message: "ok.",
+        });
+      } else {
+        throw new Error();
+      }
+    } catch (err) {
+      res.status(404).send({ message: "wineId or accessToken not found." });
+    }
+  },
+  delete: async (req: Request, res: Response) => {
+    const commentId = req.body.commentId;
+    const userId = req.session!.passport!.user;
+
+    const connection = getConnection();
+    const comment = await connection
+      .getRepository(Comment)
+      .findOne({ where: { id: commentId }, relations: ["user"] });
+    if (comment) {
+      if (comment.user.id === userId) {
+        await connection
+          .createQueryBuilder()
+          .delete()
+          .from(Recomment)
+          .where("comment = :commentId", { commentId })
+          .execute();
+
+        await connection
+          .createQueryBuilder()
+          .delete()
+          .from(Comment)
+          .where("id = :commentId", { commentId })
+          .execute();
+
+        res.status(200).send({ message: "comment successfully deleted." });
+      } else {
+        res.status(401).send({ message: "you are unauthorized." });
+      }
     } else {
+      res.status(404).send({ message: "commentId not existed." });
+    }
+  },
+  put: async (req: Request, res: Response) => {
+    const { text, commentId } = req.body;
+    const userId = req.session!.passport!.user;
+
+    const connection = await getConnection();
+    const commentRepo = await connection.getRepository(Comment);
+    const comment = await commentRepo.findOne({
+      where: { id: commentId },
+      relations: ["user"],
+    });
+    if (comment) {
+      if (comment.user.id === userId) {
+        await connection
+          .createQueryBuilder()
+          .update(Comment)
+          .set({ text: text })
+          .where("id = :commentId", { commentId })
+          .execute();
+
+        res.status(200).send({ message: "comment successfully deleted." });
+      } else {
+        res.status(401).send({ message: "you are unauthorized." });
+      }
+    } else {
+      res.status(404).send({ message: "commentId not existed." });
+    }
+  },
+  good: async (req: Request, res: Response) => {
+    try {
+      const connection = getConnection();
+      const userRepo = await connection.getRepository(User);
+      const commentRepo = await connection.getRepository(Comment);
+      const { commentId } = req.body;
+
+      //   const userId: number = req.session!.passport!.user;
+      const user = await userRepo.findOne({
+        where: { id: 2 },
+        relations: ["good"],
+      });
+      const comment = await commentRepo.findOne({ id: commentId });
+
+      console.log(user);
+    } catch (e) {
       res.status(404).send({ message: "user or commentId not existed." });
     }
   },
