23de31add1eb6801f569ee89838872b0592f3ac2
Youjin Lee <youjinlee1997@gmail.com>
2021-05-29 17:05:56 +0900

    HOTFIX: getRandomThree 타임아웃 해결 / #86

--- src/controllers/main.ts
+++ src/controllers/main.ts
@@ -15,11 +15,27 @@ const getRandomThree = (sorted: sortedWine): Wine[] => {
     .sort((a, b) => b - a);
 
   for (let key of sortedKeys) {
-    let len = sorted[key].length;
-    while (len !== 0 && random3.length < 3) {
-      let idx = Math.floor(Math.random() * len);
-      random3 = [...random3, sorted[key][idx]];
-      len--;
+    while (random3.length <= 3) {
+      if (sorted[key].length <= 3) {
+        random3 = [...sorted[key]];
+        break;
+      }
+      let result = sorted[key][Math.floor(Math.random() * sorted[key].length)]; // 랜덤으로 하나 꺼냄
+      let flag = false;
+      for (let r of random3) {
+        if (r.id === result.id) {
+          flag = true;
+        }
+      }
+      if (flag === false) {
+        random3.push(result);
+      }
+      if (random3.length === 3) {
+        break;
+      }
+    }
+    if (random3.length === 3) {
+      break;
     }
   }
   return random3;
@@ -28,10 +44,12 @@ export = {
   tags: async (req: Request, res: Response) => {
     const connection = getConnection();
     const tags: string[] = req.body.tags;
-    const sort: string[] = req.body.sort;
+    let sort: string[] = [];
+    if (req.body.sort) {
+      sort = req.body.sort;
+    }
 
     const wineRepo = await connection.getRepository(Wine);
-    const tagRepo = await connection.getRepository(Tag);
 
     let wines: Wine[] = [];
 
@@ -58,7 +76,6 @@ export = {
         .andWhere("sort IN (:...sort)", { sort })
         .getMany();
     }
-
     const sorted: sortedWine = {};
     for (let wine of wines) {
       if (sorted[wine.tags.length] === undefined) {
