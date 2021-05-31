88aafb31d7ebd7ad6b6d28591c842a5ece980874
Youjin Lee <youjinlee1997@gmail.com>
2021-05-31 21:39:43 +0900

    중간 커밋

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -10,53 +10,45 @@ require("dotenv").config();
 
 export = {
   update: async (req: Request, res: Response) => {
-    interface TokenInterface {
-      // verified accessToken의 인터페이스
-      id: number;
-      email: string;
-      nickname: string;
-      likes?: number;
-      image?: string;
-      tags: Tag[];
-      good?: Comment[];
-      bad?: Comment[];
-      wines?: Wine[];
-    }
-    const connection = await getConnection();
-    const tags: string[] = req.body.tags;
-    const accessToken: string = req.body.accessToken;
-    const { id } = jwt.verify(
-      // accessToken으로부터 유저 id를 빼내온다
-      accessToken,
-      process.env.ACCTOKEN_SECRET!
-    ) as TokenInterface;
+    try {
+      const connection = await getConnection();
+      const tags: string[] = req.body.tags;
 
-    let userRepo = connection.getRepository(User); // user table
-    let tagRepo = connection.getRepository(Tag); // tag table
+      let userId: number;
 
-    const user = await userRepo.findOne({ id });
+      // if (req.session!.passport!) {
+      //   userId = req.session!.passport!.user;
+      // } else {
+      //   throw new Error("user");
+      // }
 
-    // user가 있는 경우
-    if (user !== undefined) {
-      let tagsArr: Tag[] = [];
-      for (let tag of tags) {
-        // tag 레포에서 tag들을 찾아 tagsArr에 저장
-        const one = await tagRepo.findOne({ name: tag });
-        if (one !== undefined) {
-          await tagsArr.push(one);
+      let userRepo = connection.getRepository(User); // user table
+      let tagRepo = connection.getRepository(Tag); // tag table
+      const user = await userRepo.findOne({ id: 2 });
+      if (user !== undefined) {
+        let tagsArr: Tag[] = [];
+        for (let tag of tags) {
+          const one = await tagRepo.findOne({ name: tag });
+          if (one !== undefined) {
+            await tagsArr.push(one);
+          }
         }
-      }
-      user.tags = [...tagsArr]; // 찾은 tag들을 user_tag를 통해 user와 연결
-      await connection.manager.save(user);
+        user.tags = [...tagsArr]; // 찾은 tag들을 user_tag를 통해 user와 연결
+        await connection.manager.save(user);
 
-      if (tagsArr.length === 0) {
-        res.status(204).send({ message: "tags not found" }); // 아무런 태그를 선택하지 않은 경우
+        if (tagsArr.length === 0) {
+          res.status(204).send({ message: "tags not found" }); // 아무런 태그를 선택하지 않은 경우
+        } else {
+          res.status(200).send({ message: "ok." });
+        }
       } else {
-        res.status(200).send({ message: "ok." });
+        // user가 없는 경우
+        res.status(401).send({ message: "accessToken not existed" });
       }
-    } else {
-      // user가 없는 경우
-      res.status(401).send({ message: "accessToken not existed" });
+    } catch (e) {
+      if (e.message === "user") {
+        res.status(401).send({ message: "user not existed" });
+      }
     }
   },
   like: async (req: Request, res: Response) => {
