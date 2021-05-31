4dc8810afbd8c0f2dddcbe0b8840c1216ad78a99
Youjin Lee <youjinlee1997@gmail.com>
2021-06-01 01:00:22 +0900

    hotfix: /user/update, /user/like 부분 토큰인증=>세션인증방식으로 변경 / #100

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -12,80 +12,109 @@ export = {
   update: async (req: Request, res: Response) => {
     try {
       const connection = await getConnection();
-      const tags: string[] = req.body.tags;
+      let tags: string[] = [];
 
       let userId: number;
 
-      // if (req.session!.passport!) {
-      //   userId = req.session!.passport!.user;
-      // } else {
-      //   throw new Error("user");
-      // }
+      if (req.session!.passport!) {
+        userId = req.session!.passport!.user;
+      } else {
+        throw new Error("userId");
+      }
+
+      if (req.body.tags !== undefined && req.body.tags.length !== 0) {
+        tags = req.body.tags;
+      } else {
+        throw new Error("tags");
+      }
 
       let userRepo = connection.getRepository(User); // user table
       let tagRepo = connection.getRepository(Tag); // tag table
-      const user = await userRepo.findOne({ id: 2 });
-      if (user !== undefined) {
+      const user = await userRepo.findOne({
+        where: { id: userId },
+        relations: ["tags"],
+      });
+
+      if (user) {
         let tagsArr: Tag[] = [];
         for (let tag of tags) {
           const one = await tagRepo.findOne({ name: tag });
           if (one !== undefined) {
-            await tagsArr.push(one);
+            tagsArr.push(one);
           }
         }
-        user.tags = [...tagsArr]; // 찾은 tag들을 user_tag를 통해 user와 연결
-        await connection.manager.save(user);
 
-        if (tagsArr.length === 0) {
-          res.status(204).send({ message: "tags not found" }); // 아무런 태그를 선택하지 않은 경우
-        } else {
-          res.status(200).send({ message: "ok." });
+        for (let tag of user.tags) {
+          await connection
+            .createQueryBuilder()
+            .relation(User, "tags")
+            .of(user)
+            .remove(tag);
         }
+        await connection
+          .createQueryBuilder()
+          .relation(User, "tags")
+          .of(user)
+          .add(tagsArr);
+
+        res.status(200).send({ message: "ok." });
       } else {
-        // user가 없는 경우
-        res.status(401).send({ message: "accessToken not existed" });
+        throw new Error("user");
       }
     } catch (e) {
-      if (e.message === "user") {
+      if (e.message === "userId") {
+        res.status(401).send({ message: "userId not existed" });
+      } else if (e.message === "tags") {
+        res.status(204).send({ message: "tags not found" }); // 아무런 태그를 선택하지 않은 경우
+      } else if (e.messageb === "user") {
         res.status(401).send({ message: "user not existed" });
       }
     }
   },
   like: async (req: Request, res: Response) => {
-    interface TokenInterface {
-      // verified accessToken의 인터페이스
-      id: number;
-      email: string;
-      nickname: string;
-      likes?: number;
-      image?: Buffer;
-      tags: Tag[];
-      good?: Comment[];
-      bad?: Comment[];
-      wines?: Wine[];
-    }
-    const wineId: number = req.body.wineId;
-    const accessToken: string = req.body.accessToken;
-    const userinfo = jwt.verify(
-      accessToken,
-      process.env.ACCTOKEN_SECRET!
-    ) as TokenInterface;
+    try {
+      let wineId: number;
+      let userId: number;
 
-    const connection = await getConnection(); // 데이터베이스와 연결
-    const wineRepo = await connection.getRepository(Wine);
-    const userRepo = await connection.getRepository(User);
+      if (req.session!.passport!) {
+        userId = req.session!.passport!.user;
+      } else {
+        throw new Error("userId");
+      }
+      if (req.body.wineId) {
+        wineId = req.body.wineId;
+      } else {
+        throw new Error("wineId");
+      }
 
-    const wine: Wine | undefined = await wineRepo.findOne({ id: wineId });
-    const user: User | undefined = await userRepo.findOne({ id: userinfo.id });
+      const connection = await getConnection(); // 데이터베이스와 연결
+      const wineRepo = await connection.getRepository(Wine);
+      const userRepo = await connection.getRepository(User);
 
-    if (!userinfo || !user) {
-      res.status(401).send({ message: "accessToken not existed" });
-    } else if (wine === undefined || !wine) {
-      res.status(404).send({ message: "wine not existed" });
-    } else {
-      user.wines = [...user.wines, wine];
-      await userRepo.save(user);
-      res.status(200).send({ message: "ok" });
+      const wine: Wine | undefined = await wineRepo.findOne({ id: wineId });
+      const user: User | undefined = await userRepo.findOne({ id: userId }); // 2=>userId
+      if (wine && user) {
+        await connection
+          .createQueryBuilder()
+          .relation(User, "wines")
+          .of(user)
+          .add(wine);
+        res.status(200).send({ message: "ok" });
+      } else if (!user) {
+        throw new Error("user");
+      } else if (!wine) {
+        throw new Error("wine");
+      }
+    } catch (e) {
+      if (e.message === "userId") {
+        res.status(401).send({ message: "you are unauthorized" });
+      } else if (e.message === "wineId") {
+        res.status(404).send({ message: "wineId not existed" });
+      } else if (e.message === "user") {
+        res.status(404).send({ message: "user not existed" });
+      } else if (e.message === "wine") {
+        res.status(404).send({ message: "wine not existed" });
+      }
     }
   },
   unlike: async (req: Request, res: Response) => {
