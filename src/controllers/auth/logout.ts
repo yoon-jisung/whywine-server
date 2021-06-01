131826b0c60cd258fc751932488f16fe734cadd0
jeasong <cjs5241@gmail.com>
2021-06-01 18:10:16 +0900

    디버깅: 로그아웃 핫픽스

--- src/controllers/auth/logout.ts
+++ src/controllers/auth/logout.ts
@@ -3,10 +3,14 @@ import { Router, Request, Response, NextFunction } from "express";
 const logout = async (req: Request, res: Response) => {
     console.log("로그 아웃");
     req.logout();
+    if (req.session) {
     req.session.destroy((err) => {
         res.clearCookie('connect.sid');
         res.send({ data: null, message: 'logout success' });
     });
+    } else {
+        res.send({ data: null, message: 'logout success' });
+    }
 }
 
 export default logout;
