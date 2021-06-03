af53ecbfb8e2f05118d8b9d09d71ff90b2ac1382
yoon-jisung <hmsj2380@gmail.com>
2021-06-03 16:33:28 +0900

    update

--- src/utils/strategy/google.ts
+++ src/utils/strategy/google.ts
@@ -1,47 +1,57 @@
-import passport from 'passport';
+import passport from "passport";
 import { createQueryBuilder, getRepository } from "typeorm";
 import { User } from "../../entity/user";
-import dotenv from 'dotenv';
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
+import dotenv from "dotenv";
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
 dotenv.config();
-const server = process.env.SERVER || 'https://localhost:4000'
+const server = process.env.SERVER || "https://localhost:4000/";
 export default () => {
-    passport.use(
-        new GoogleStrategy(
-            {
-                clientID: process.env.GOOGLE_CLIENT_ID,
-                clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-                callbackURL: `${server}/auth/google/callback`
-            },
-            async function (accessToken:string, refreshToken:string, profile:any, cb:any) {
-                
-                try {
-                    console.log(profile)
-                    const { _json: { email, name, picture, sub} } = profile;
-                    
-                    const users = (await createQueryBuilder("user")
-                        .where("email = :email", { email })
-                        .execute());
-                    for (let user of users) {
-                        if (user && user.User_password == null && user.User_email.split('@')[1] === 'gmail.com') {
-                            return cb(null, user)
-                        }
-                    }
-                    const userRepository = await getRepository(User);
-                    console.log('구글 처음 로그인')
-                    const userInfo = new User
-                    userInfo.email = email
-                    userInfo.nickname = name
-                    userInfo.likes = 0
-                    userInfo.image = picture
-                    const saveData = await userRepository.save(userInfo)
-                    const userId = { User_id: saveData.id }
-                    return cb(null,userId);
-                } catch (error) {
-                    console.log('에러',error)
-                    return cb(error)
-                }
+  passport.use(
+    new GoogleStrategy(
+      {
+        clientID: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        callbackURL: `${server}auth/google/callback`,
+      },
+      async function (
+        accessToken: string,
+        refreshToken: string,
+        profile: any,
+        cb: any
+      ) {
+        try {
+          console.log(profile);
+          const {
+            _json: { email, name, picture, sub },
+          } = profile;
+
+          const users = await createQueryBuilder("user")
+            .where("email = :email", { email })
+            .execute();
+          for (let user of users) {
+            if (
+              user &&
+              user.User_password == null &&
+              user.User_email.split("@")[1] === "gmail.com"
+            ) {
+              return cb(null, user);
             }
-        )
+          }
+          const userRepository = await getRepository(User);
+          console.log("구글 처음 로그인");
+          const userInfo = new User();
+          userInfo.email = email;
+          userInfo.nickname = name;
+          userInfo.likes = 0;
+          userInfo.image = picture;
+          const saveData = await userRepository.save(userInfo);
+          const userId = { User_id: saveData.id };
+          return cb(null, userId);
+        } catch (error) {
+          console.log("에러", error);
+          return cb(error);
+        }
+      }
     )
-}
+  );
+};
--- src/utils/strategy/kakao.ts
+++ src/utils/strategy/kakao.ts
@@ -1,46 +1,62 @@
-import dotenv from 'dotenv';
-import passport from 'passport';
+import dotenv from "dotenv";
+import passport from "passport";
 import { User } from "../../entity/user";
 import { createQueryBuilder, getRepository } from "typeorm";
-const KakaoStrategy = require('passport-kakao').Strategy;
+const KakaoStrategy = require("passport-kakao").Strategy;
 dotenv.config();
-const server = process.env.SERVER || 'https://localhost:4000'
+const server = process.env.SERVER || "https://localhost:4000/";
 export default () => {
-    passport.use(new KakaoStrategy({
-        clientID:     process.env.KAKAO_CLIENT_ID,
-        callbackURL: `${server}/auth/kakao/callback`,
-    },async function(accessToken:string, refreshToken:string, profile:any, cb:any){
-                        
+  passport.use(
+    new KakaoStrategy(
+      {
+        clientID: process.env.KAKAO_CLIENT_ID,
+        callbackURL: `${server}auth/kakao/callback`,
+      },
+      async function (
+        accessToken: string,
+        refreshToken: string,
+        profile: any,
+        cb: any
+      ) {
         try {
-            const { _json: { id, properties: {nickname, profile_image, thumbnail_image } } } = profile;
-            
-            const users = (await createQueryBuilder("user")
-            .where("email = :email", { email: "Kakao " + id })
-            .execute());
+          const {
+            _json: {
+              id,
+              properties: { nickname, profile_image, thumbnail_image },
+            },
+          } = profile;
 
-            for (let user of users) {
-                if (user && user.User_password == null && user.User_email.split(' ')[0] === 'Kakao') {
-                    return cb(null, user)
-                }
+          const users = await createQueryBuilder("user")
+            .where("email = :email", { email: "Kakao " + id })
+            .execute();
+
+          for (let user of users) {
+            if (
+              user &&
+              user.User_password == null &&
+              user.User_email.split(" ")[0] === "Kakao"
+            ) {
+              return cb(null, user);
             }
-            const userRepository = await getRepository(User);
-            console.log('카카오 처음 로그인')
-            
-            
-            const userInfo = new User
-            userInfo.email = "Kakao " + id
-            userInfo.nickname = nickname
-            userInfo.likes = 0
-            userInfo.image = profile_image
-            const saveData = await userRepository.save(userInfo)
-            
-            const userId = { User_id: saveData.id }
-            
-            return cb(null,userId);
+          }
+          const userRepository = await getRepository(User);
+          console.log("카카오 처음 로그인");
+
+          const userInfo = new User();
+          userInfo.email = "Kakao " + id;
+          userInfo.nickname = nickname;
+          userInfo.likes = 0;
+          userInfo.image = profile_image;
+          const saveData = await userRepository.save(userInfo);
+
+          const userId = { User_id: saveData.id };
+
+          return cb(null, userId);
         } catch (error) {
-            console.log('에러',error)
-            return cb(error)
+          console.log("에러", error);
+          return cb(error);
         }
-    }
-    ));
-}
+      }
+    )
+  );
+};
