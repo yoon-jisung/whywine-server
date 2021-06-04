11a227bcb61166de1cbe93afccf4463bbaafaaba
yoon-jisung <hmsj2380@gmail.com>
2021-06-05 01:59:04 +0900

    updete

--- src/utils/strategy/google.ts
+++ src/utils/strategy/google.ts
@@ -6,7 +6,6 @@ const GoogleStrategy = require("passport-google-oauth20").Strategy;
 dotenv.config();
 const server = process.env.SERVER || "https://localhost:4000/";
 export default () => {
-<<<<<<< HEAD
   passport.use(
     new GoogleStrategy(
       {
@@ -36,41 +35,6 @@ export default () => {
               user.User_email.split("@")[1] === "gmail.com"
             ) {
               return cb(null, user);
-=======
-    passport.use(
-        new GoogleStrategy(
-            {
-                clientID: process.env.GOOGLE_CLIENT_ID,
-                clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-                callbackURL: `${server}/auth/google/callback`
-            },
-            async function (accessToken:string, refreshToken:string, profile:any, cb:any) {
-                try {
-                    const { _json: { email, name, picture, sub} } = profile;
-                    
-                    const users = (await createQueryBuilder("user")
-                        .where("email = :email", { email })
-                        .execute());
-                    for (let user of users) {
-                        if (user && user.User_password == null && user.User_email.split('@')[1] === 'gmail.com') {
-                            return cb(null, user)
-                        }
-                    }
-                    const userRepository = await getRepository(User);
-                    console.log('구글 처음 로그인')
-                    const userInfo = new User
-                    userInfo.email = email
-                    userInfo.nickname = name
-                    userInfo.likes = 0
-                    userInfo.image = picture
-                    const saveData = await userRepository.save(userInfo)
-                    const userId = { User_id: saveData.id }
-                    return cb(null,userId);
-                } catch (error) {
-                    console.log('에러',error)
-                    return cb(error)
-                }
->>>>>>> 77a7e98ba32b57500e175f39c9f8a2b92e82b63d
             }
           }
           const userRepository = await getRepository(User);
